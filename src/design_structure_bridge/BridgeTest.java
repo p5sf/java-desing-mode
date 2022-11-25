f024fe49a3f2e17d0ad25b2b31c81e4f20634319
yanzhao <1770285990@qq.com>
2022-11-25 13:23:29 +0800

    桥接模式

--- src/design_structure_bridge/BridgeTest.java
+++ src/design_structure_bridge/BridgeTest.java
@@ -4,15 +4,15 @@ import java.math.BigDecimal;
 
 /**
  * @author YanZhao
- * @description 逻辑测试类
+ * @description 桥接模式
  * @date 2022年11月21日 20:43
  */
 
 public class BridgeTest {
     public static void main(String[] args) {
         WxPay pay = new WxPay(new PayFaceMode());
-        pay.transfer("1000", new BigDecimal(100));
-        System.out.println("微信支付+人脸支付");
+        String transfer = pay.transfer("1000", new BigDecimal(100));
+        System.out.println("支付方式："+transfer);
 
     }
 }
